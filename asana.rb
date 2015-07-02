90aea544dbb884a9fe957c9377ace968ccd7d381
Alex Kitchens <alexcameron98@gmail.com>
2015-07-02 17:08:52 -0500

    Refactor: moves procedural code to methods

--- asana.rb
+++ asana.rb
@@ -4,8 +4,9 @@ require "rubygems"
 require "json"
 require "net/https"
 require "csv"
-api_key = "2eSUdgF2.wSgSYfK9hqcVz70LFiWPgg5"
-task_sheet = ARGV.shift
+API_KEY = "2eSUdgF2.wSgSYfK9hqcVz70LFiWPgg5"
+sheet_loc = ARGV.shift
+task_sheet = CSV.read(sheet_loc)
 
 def add_task
 end
@@ -13,7 +14,7 @@ end
 def add_subtask
 end
 
-#def set_https_connection
+def set_https_connection
   # set up HTTPS connection
   uri = URI.parse("https://app.asana.com/api/1.0/tasks")
   http = Net::HTTP.new(uri.host, uri.port)
@@ -25,16 +26,10 @@ end
     "Content-Type" => "application/json"
   }
 
-#  return uri, http, header
-#end
-
-def structure_row_content(row)
-  
+  return uri, http, header
 end
 
-#uri, http, header = set_https_connection
-
-CSV.foreach(task_sheet) do |row|
+def structure_row_content(row, uri)
   data = Hash.new
   if row[0] == "subtask"
     uri.path = "/api/1.0/tasks/#{row[1]}/subtasks"
@@ -46,8 +41,12 @@ CSV.foreach(task_sheet) do |row|
   data["name"] = row[2]
   data["notes"] = row[3]
 
+  return data
+end
+
+def issue_request(uri, header, http, data)
   req = Net::HTTP::Post.new(uri.path, header)
-  req.basic_auth(api_key, '')
+  req.basic_auth(API_KEY, '')
   req.body = {
     "data" => data
   }.to_json()
@@ -55,8 +54,20 @@ CSV.foreach(task_sheet) do |row|
   # issue the request
   res = http.start { |http| http.request(req) }
 
+  return res
+end
+
+uri, http, header = set_https_connection
+columns = task_sheet.shift
+
+while task_sheet.length > 0
+  data = structure_row_content(task_sheet.pop, uri)
+
+  response = issue_request(uri, header, http, data)
+
+
   # output
-  body = JSON.parse(res.body)
+  body = JSON.parse(response.body)
   if body['errors'] then
     puts "Server returned an error: #{body['errors'][0]['message']}"
   else
